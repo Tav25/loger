c4e8612cca1af34a9f13cc391619d2e1e4781c8a
Tav25 <turbin28@gmail.com>
2018-05-31 14:31:52 +0300

    h

--- LOGGER.py
+++ LOGGER.py
@@ -20,24 +20,53 @@ def sek_to_time_ND(sek):
     a = time.strftime("%H:%M:%S", time.gmtime(sek))
     return a
 
-def j_load():
+def j_load(file_data):
     myfile = open(file_data, 'r')
     global process_name
     process_name = json.load(myfile)
     myfile.close()
 
-def j_dump():
+def j_dump(file_data):
     global process_name
     myfile = open(file_data, 'w')
     json.dump(process_name, myfile)
     myfile.close()
 
+#
+def clar_json(file_data):
+    j_load(file_data)
+    for g in process_name:
+        print(g)
+        process_name[g].clear()
+    j_dump()
+
+def obr():
+    j_load(file_data)
 
 
-while True:
+
+#while True:
+x = int(time.time())
+print(x)
+#obr()
+j_load(file_data)
+
+for g in process_name:
+    process_name[g]['id'] = 0
+    process_name[g]['time_run'] = 0
+    process_name[g]['time_now'] = 0
+    #print(g)
+for g in process_name:
+    for proc in psutil.process_iter():
+        name = proc.name()
+        if name == g:
+            print(name)
+            process_name[g]['id'] = proc.pid
+            process_name[g]['time_run'] = proc.create_time()
+            process_name[g]['time_now'] = x - process_name[g]['time_run']
 
 
-    
-    
-    time.sleep(1)
-    os.system('cls')
+print(process_name)
+
+#time.sleep(1)
+#os.system('cls')
